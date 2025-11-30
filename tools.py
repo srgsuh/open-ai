de1715bfcca127cd375503bb827a30e8a05b2fa6
srgsuh <srg.suhoverhov@gmail.com>
2025-11-30 14:15:47 +0200

    Minor refactoring

--- tools.py
+++ tools.py
@@ -6,6 +6,13 @@ from datetime import datetime
 __placeholder: str = "unknown"
 __empty_dict: dict[str, Any] = {}
 
+#weather data dictionary keys:
+__CITY: str = "city"
+__TEMP: str = "temp"
+__CONDITION: str = "condition"
+__HUMIDITY: str = "humidity"
+__WIND: str = "wind_speed"
+
 class UnknownLocationError(RuntimeError):
     """City name not found"""
 
@@ -21,27 +28,27 @@ def __get_api_response(city_name: str) -> dict[str, Any]:
     response.raise_for_status()
     return response.json()
 
-def __parse_response(json_data: dict[str, Any]) -> dict[str, str]:
+def __parse_response(json_data: dict[str, Any]) -> dict[str, str | None]:
     location: dict[str, Any] = json_data.get("location", __empty_dict)
     current_weather: dict[str, Any] = json_data.get("current", __empty_dict)
     if not location or not current_weather:
         raise UnknownLocationError("Unknown city name")
     
     return {
-        "city": location.get("name", ""),
-        "temperature": current_weather.get("temp_c", ""),
-        "condition": current_weather.get("condition", __empty_dict).get("text", ""),
-        "humidity": current_weather.get("humidity", ""),
-        "wind speed": current_weather.get("wind_kph", "")
+        __CITY: location.get("name"),
+        __TEMP: current_weather.get("temp_c", __placeholder),
+        __CONDITION: current_weather.get("condition", __empty_dict).get("text", __placeholder),
+        __HUMIDITY: current_weather.get("humidity", __placeholder),
+        __WIND: current_weather.get("wind_kph", __placeholder)
     }
 
-def __weather_data_to_str(data: dict[str, str]) -> str:
+def __weather_data_to_str(data: dict[str, str | None]) -> str:
     today: str = datetime.now().strftime("%d.%m.%Y")
-    city = data["city"]
-    temperature = data["temperature"] if data["temperature"] is not None else __placeholder
-    condition = data["condition"] if data["condition"] is not None else __placeholder
-    humidity = data["humidity"] if data["humidity"] is not None else __placeholder
-    wind_speed = data["wind speed"] if data["wind speed"] is not None else __placeholder
+    city = data[__CITY]
+    temperature = data[__TEMP]
+    condition = data[__CONDITION]
+    humidity = data[__HUMIDITY]
+    wind_speed = data[__WIND]
     return (
         f"Weather in {city} on {today}: "
         f"{temperature}°C, {condition}. "
@@ -53,7 +60,7 @@ def get_weather(city_name: str) -> str:
     description: str = ""
     try:
         json_data: dict[str, Any] = __get_api_response(city_name)
-        weather_data: dict[str, str] = __parse_response(json_data)
+        weather_data: dict[str, str | None] = __parse_response(json_data)
         description = __weather_data_to_str(weather_data)
     except Exception as e:
         description = f"Weather forecast in {city_name} is unavailable. Error: {str(e)}"
@@ -61,5 +68,4 @@ def get_weather(city_name: str) -> str:
     return description
 
 if __name__ == "__main__":
-    print(get_weather("11"))
-    print(get_weather("oooooooooopsss"))
+    print(get_weather("Los Angeles"))
