72a8a9283da62ebc6538534015d7e987d85d63c7
srgsuh <srg.suhoverhov@gmail.com>
2025-11-30 09:34:10 +0200

    Add json parsing

--- tools.py
+++ tools.py
@@ -21,3 +21,17 @@ def __get_api_response(city_name: str) -> dict[str, Any]:
     response.raise_for_status()
     return response.json()
 
+def __parse_response(data: dict[str, Any]) -> dict[str, str]:
+    location: dict[str, Any] = data.get("location", __empty_dict)
+    current_weather: dict[str, Any] = data.get("current", __empty_dict)
+    if not location or not current_weather:
+        raise UnknownLocationError("Unknown city name")
+    
+    return {
+        "city": location.get("name", ""),
+        "temperature": current_weather.get("temp_c", ""),
+        "condition": current_weather.get("condition", __empty_dict).get("text", ""),
+        "humidity": current_weather.get("humidity", ""),
+        "wind speed": current_weather.get("wind_kph", "")
+    }
+
