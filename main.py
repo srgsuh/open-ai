2819c676305feeaa8e6ef5b51462bff7d503633d
srgsuh <srg.suhoverhov@gmail.com>
2025-12-03 11:26:13 +0200

    Add basic chat model

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import requests
-from typing import Any
+from typing import Any, Callable
+from tools import TOOLS
+from system_content import SYSTEM_CONTENT
+import re
+import json
+from thinking_dots import start_thinking_dots
+import threading
 
 URL: str = "http://localhost:11434/api/chat"
 MODEL_NAME: str = "phi3"
@@ -8,18 +14,58 @@ def chat_request(messages: list) -> str:
     payload: dict[str, Any] = {
         "model": MODEL_NAME,
         "messages": messages,
-        "stream": False
+        "stream": False,
+        "options": {"temperature": 0.05}
     }
     response = requests.post(URL, json=payload)
     response.raise_for_status()
     data = response.json()
     return data["message"]["content"]
 
+def extract_json(text: str) -> dict | None:
+    json_re: str = r"\{.*tool.*arguments.*\}"
+    match: re.Match | None = re.search(json_re, text, re.DOTALL)
+    
+    result: dict | None = None
+    if match:
+        try:
+            result = json.loads(match.group())
+        except Exception:
+            pass
+    
+    return result
+
+def call_tool(tool_data: dict) -> str:
+    result: str = ""
+    callable: Callable | None = TOOLS.get(tool_data.get("tool", ""))
+    if callable:
+        arguments: dict = tool_data.get("arguments", {})
+        result = callable(**arguments)
+    
+    return result
+
+def process_LLM(text: str) -> dict:
+    res: dict = {
+        "role": "assistant",
+        "content": text
+    }
+    tool_data: dict | None = extract_json(text)
+    if tool_data:
+        tool_response: str = call_tool(tool_data)
+        if tool_response:
+            res = {
+                "role": "tool",
+                "content": json.dumps({"result": tool_response})
+            }
+
+    return res
+
+
 if __name__ == "__main__":
     messages = [
         {
             "role": "System",
-            "content": "You should answer clearly and briefly."
+            "content": SYSTEM_CONTENT
         }
     ]
     chat_request(messages)
@@ -33,10 +79,10 @@ if __name__ == "__main__":
             "role": "user",
             "content": user_input
         })
-        reply = chat_request(messages)
-        messages.append({
-            "role": "assistant",
-            "content": reply
-        })
-        print(f"\nAgent: {reply}")
+        stop_event: threading.Event = start_thinking_dots("Model is thinking", 0.5)
+        reply: str = chat_request(messages)
+        response: dict = process_LLM(reply)
+        stop_event.set()
+        messages.append(response)
+        print(f"\nAgent: {response["content"]}")
         print("_"*60)
