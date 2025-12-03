d0c4249d827f4771bd2942cb0f4e85c945c55c89
srgsuh <srg.suhoverhov@gmail.com>
2025-12-03 12:14:05 +0200

    Add default parameter to get_parameter function

--- configuration.py
+++ configuration.py
@@ -6,8 +6,8 @@ load_dotenv()
 class ConfigurationError(RuntimeError):
     pass
 
-def get_config_parameter(parameter_name: str) -> str:
-    parameter = os.getenv(parameter_name)
+def get_config_parameter(parameter_name: str, default_value: str | None = None) -> str:
+    parameter: str | None = os.getenv(parameter_name, default=default_value)
     if parameter is None:
         raise ConfigurationError(f"Missing configuration for the parameter: {parameter_name}")
     return parameter
