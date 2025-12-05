96f43e77a7a7fc9d6561c8767d23f8853aa7e9b4
srgsuh <srg.suhoverhov@gmail.com>
2025-12-05 14:50:30 +0200

    Refactor currency rate into class

--- fixer.py
+++ fixer.py
@@ -1,43 +1,57 @@
 import requests
 from configuration import get_config_parameter
+from datetime import datetime
 
 SUCCESS_KEY: str = "success"
 RATES_KEY: str = "rates"
+DATE_KEY: str = "date"
+FIXER_DATE_FMT: str = "%Y-%m-%d"
 
 class CurrencyRequestError(RuntimeError):
     pass
 
-def get_url() -> str:
+def __get_url() -> str:
     return get_config_parameter("FIXER_URL")
 
-def get_query_params() -> dict[str, str]:
-    api_key: str = get_config_parameter("FIXER_API_KEY")
-    return {"access_key" : api_key}
+def __get_api_key() -> str:
+    return get_config_parameter("FIXER_API_KEY")
 
-def get_latest_raw() -> dict:
-    response = requests.get(get_url(), params=get_query_params())
-    response.raise_for_status()
-    return response.json()
-
-def get_latest() -> dict:
-    raw_response: dict = get_latest_raw()
-    if not raw_response[SUCCESS_KEY]:
-        raise CurrencyRequestError("Currency data is unavailable now. Try again later.")
+class CurrencyRate:
+    def __init__(self, url, api_key) -> None:
+        self.__url = url
+        self.__api_key = api_key
+        self.__latest_date: str = ""
+        self.__latest_cache: dict = {}
     
-    return raw_response[RATES_KEY]
+    def __get_latest_raw(self) -> dict:
+        response = requests.get(self.__url, params={"access_key" : self.__api_key})
+        response.raise_for_status()
+        return response.json()
 
-def calculate_rate(euro_rates: dict, code_from: str, code_to: str) -> float:
-    def get_euro_rate(code: str) -> float:
-        if not code in euro_rates:
-            raise ValueError(f"Currency code {code} is not supported")
-        return euro_rates[code]
+    def __load_rates(self) -> None:
+        raw_response: dict = self.__get_latest_raw()
+        if not raw_response[SUCCESS_KEY]:
+            raise CurrencyRequestError("Currency data is unavailable now. Try again later.")
+        
+        self.__latest_date = raw_response[DATE_KEY]
+        self.__latest_cache = raw_response[RATES_KEY]
+
+    def __get_latest(self) -> None:
+        today: str = datetime.now().strftime(FIXER_DATE_FMT)
+        if not (self.__latest_date == today):
+            self.__load_rates()
+
+    def get_rate(self, code_from: str, code_to: str) -> float:
+        self.__get_latest()
+        def get_euro_rate(code: str) -> float:
+            if not code in self.__latest_cache:
+                raise ValueError(f"Currency code {code} is not supported")
+            return self.__latest_cache[code]
     
-    return get_euro_rate(code_to) / get_euro_rate(code_from)
+        return get_euro_rate(code_to) / get_euro_rate(code_from)
 
-def get_rate(code_from: str, code_to: str) -> float:
-    latest: dict = get_latest()
-    return calculate_rate(euro_rates=latest, code_from=code_from, code_to=code_to)
+CURRENCY_RATE: CurrencyRate = CurrencyRate(__get_url(), __get_api_key())
 
 if __name__ == "__main__":
-    print(get_latest())
-    print(get_rate('USD', 'GBP'))
+    rate: float = CURRENCY_RATE.get_rate('USD', 'GBP')
+    print(f"1 dollar = {rate} pounds")
--- fixer_integration_test.py
+++ fixer_integration_test.py
@@ -1,22 +1,31 @@
 import pytest
 import responses
-from fixer import get_rate, SUCCESS_KEY, RATES_KEY
+from fixer import CurrencyRate, SUCCESS_KEY, RATES_KEY, DATE_KEY, FIXER_DATE_FMT
+from datetime import datetime
 import json
 
 __mock_key: str = "key"
 __mock_url: str = "http://mock.mock"
 
 @responses.activate
-def test_get_rate_success(monkeypatch) -> None:
-    monkeypatch.setenv("FIXER_URL", __mock_url)
-    monkeypatch.setenv("FIXER_API_KEY", __mock_key)
+def test_get_rate_success() -> None:
+    currencyRate: CurrencyRate = CurrencyRate(__mock_url, __mock_key)
+
     key1, key2 = "key1", "key2"
     __mock_rates: dict[str, float] = { key1: 2.0, key2: 3.0 }
     __mock_response: dict = {
         SUCCESS_KEY: True,
-        RATES_KEY: __mock_rates
+        RATES_KEY: __mock_rates,
+        DATE_KEY: datetime.now().strftime(FIXER_DATE_FMT)
     }
     responses.add(responses.GET, url=__mock_url, json=__mock_response, status=200)
 
-    rate: float = get_rate(key1, key2)
+    rate: float = currencyRate.get_rate(key1, key2)
     assert rate == pytest.approx(__mock_rates[key2] / __mock_rates[key1])
+
+@responses.activate
+def test_get_rate_network_error() -> None:
+    currencyRate: CurrencyRate = CurrencyRate(__mock_url, __mock_key)
+    responses.add(responses.GET, url=__mock_url, status=400)
+    with pytest.raises(Exception):
+        currencyRate.get_rate("1", "2")
