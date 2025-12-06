3515643ab1cbbf0944e1070e376f274c63485c5d
srgsuh <srg.suhoverhov@gmail.com>
2025-12-06 10:32:02 +0200

    Simplify the implementation

--- extract_json.py
+++ extract_json.py
@@ -1,28 +1,21 @@
 import re
 
 def extract_json(source_str: str, words: list[str] = []) -> str:
-    look_ahead: str = ""
-    if words:
-        look_ahead = "".join([f"(?=.*{re.escape(word)})" for word in words])
-    
-    pattern: str = rf"{look_ahead}(\{{.*\}})"
+    look_ahead = "".join([f"(?=.*{re.escape(word)})" for word in words])
+    pattern: str = rf"{look_ahead}\{{.*\}}"
     match: re.Match | None = re.search(pattern=pattern, string = source_str, flags=re.DOTALL)
 
-    json_str: str = ""
-    if match:
-        json_str = match.group(1)
-
-    return json_str
+    return "" if match is None else match.group(0)
 
 if __name__ == "__main__":
     string: str = """
-    Hi there this is JSON: {
-        "role": "player",
+    JSON example: {
+        "role": "user",
         "content": {
-            "foo": "value"}
+            "key": "value"}
             }
     }
     some text there too.
     """
 
-    print(extract_json(string, ["content", "role"]))
+    print(extract_json(string, ["content", "key", "role"]))
