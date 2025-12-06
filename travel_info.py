6acc689f8a30ab81cd86d0e5b47eff839718f6f6
srgsuh <srg.suhoverhov@gmail.com>
2025-12-06 18:31:12 +0200

    Integrate exchange-rate retrieval into the travel_info function

--- travel_info.py
+++ travel_info.py
@@ -4,16 +4,24 @@ import json
 from logs import debug
 from chat_request import ChatLLM
 from extract_json import extract_json
+from fixer import CURRENCY_RATE
 
 def travel_info(country_from: str, country_to: str, code_from: str) -> str:
-    chat = ChatLLM(INNER_SYSTEM_CONTENT)
-    chat.user_message(f"The currency of {country_to}")
-
-    debug(f"Requesting data about {country_to}")
-    raw_reply: str = chat.request()
-    debug(f"Data about {country_to} = {raw_reply}")
-    json_reply: dict = extract_json(raw_reply, ["country", "currency_name", "currency_code"]) or {}
-    debug(f"Parsed data about {country_to} = {json_reply}")
-    code_to: str = json_reply.get("currency_code", "unknown")
-        
-    return (f"Travel from {country_from} to {country_to}, code_from={code_from}, code_to={code_to}")
+    result: dict[str, str | float]
+    try:
+        chat = ChatLLM(INNER_SYSTEM_CONTENT, temperature=0.0)
+        raw_reply: str = chat.user_message(f"The currency of {country_to}").request()
+        json_reply: dict = extract_json(raw_reply, ["country", "currency_name", "currency_code"]) or {}
+        code_to: str = json_reply["currency_code"]
+        rate = CURRENCY_RATE.get_rate(code_from, code_to)
+        result = {
+            "country_from": country_from,
+            "code_from": code_from,
+            "country_to": country_to,
+            "code_to": code_to,
+            "exchange_rate": rate
+        }
+    except Exception as e:
+        result = {"error" : str(e)}
+    
+    return json.dumps(result)
