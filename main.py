85b937fb7d85bd1a14dac78f22c6d53cfe67980b
srgsuh <srg.suhoverhov@gmail.com>
2025-12-06 19:18:14 +0200

    Add try-catch block to stop printning thinking dots

--- main.py
+++ main.py
@@ -5,21 +5,27 @@ from system_rules import APP_SYSTEM_CONTENT
 from chat_request import ChatLLM
 from process_llm import process_LLM
 
+def is_exit_request(user_input: str) -> bool:
+    return user_input.lower() in ['exit', 'stop', 'quit', 'q']
+
 if __name__ == "__main__":
     debug("Start")
     chat: ChatLLM = ChatLLM(APP_SYSTEM_CONTENT, temperature=0.0)
-    chat.request()
     print("Starting a phi3 chat. Type 'exit' to quit.")
     debug("Chat is started")
     while(True):
         user_input: str = input("You: ")
-        if (user_input.lower() == 'exit'):
+        if is_exit_request(user_input):
             print("Closing the chat. Thank you. Bye!")
             debug("Chat is closed")
             break
         chat.user_message(user_input)
-        stop_event: threading.Event = start_thinking_dots("Model is thinking", 0.5)
-        response: str = process_LLM(chat)
-        stop_event.set()
+        try:
+            stop_event: threading.Event = start_thinking_dots("Model is thinking", 0.5)
+            response: str = process_LLM(chat)
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+        finally:
+            stop_event.set()
         print(f"\nAgent: {response}")
         print("_"*60)
