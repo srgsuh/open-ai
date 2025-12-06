bc578c234cd4a13472751915576af27257ad693f
srgsuh <srg.suhoverhov@gmail.com>
2025-12-06 10:39:23 +0200

    Add method to extract a dictionary from a JSON-containing string

--- extract_json.py
+++ extract_json.py
@@ -1,21 +1,31 @@
 import re
+import json
 
-def extract_json(source_str: str, words: list[str] = []) -> str:
+def extract_json_str(source_str: str, words: list[str] = []) -> str:
     look_ahead = "".join([f"(?=.*{re.escape(word)})" for word in words])
     pattern: str = rf"{look_ahead}\{{.*\}}"
     match: re.Match | None = re.search(pattern=pattern, string = source_str, flags=re.DOTALL)
 
     return "" if match is None else match.group(0)
 
+def extract_json(source_str: str, words: list[str] = []) -> dict | None:
+    result: dict | None = None
+    try:
+        result = json.loads(extract_json_str(source_str, words))
+    except Exception:
+        pass
+
+    return result
+
 if __name__ == "__main__":
     string: str = """
     JSON example: {
         "role": "user",
         "content": {
-            "key": "value"}
-            }
+            "key": "value"
+        }
     }
     some text there too.
     """
-
+    
     print(extract_json(string, ["content", "key", "role"]))
