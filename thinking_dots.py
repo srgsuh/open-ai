9eeebb66f44115182785fde8849b177ec59b7b28
srgsuh <srg.suhoverhov@gmail.com>
2025-12-06 20:46:30 +0200

    Extract method to print to console

--- thinking_dots.py
+++ thinking_dots.py
@@ -5,6 +5,10 @@ from typing import Self
 
 MAX_DOTS: int = 20
 
+def send_to_console(string: str) -> None:
+    sys.stdout.write(string)
+    sys.stdout.flush()
+
 class ThinkingDots:
     def __init__(self, label: str, interval: float = 0.5, dots: int = MAX_DOTS):
         self.label = label
@@ -22,16 +26,13 @@ class ThinkingDots:
     def __start_thinking_dots(self) -> threading.Event:
         stop_event = threading.Event()
         def worker() -> None:
-            sys.stdout.write(self.label)
-            sys.stdout.flush()
-            dots = 0
+            send_to_console(self.label)
+            dots: int = 0
             while not stop_event.is_set():
-                sys.stdout.write(".")
-                sys.stdout.flush()
+                send_to_console(".")
                 dots += 1
                 if dots > self.dots:
-                    sys.stdout.write("\n"+self.label)
-                    sys.stdout.flush()
+                    send_to_console("\n"+self.label)
                     dots = 0
                 time.sleep(self.interval)
         
