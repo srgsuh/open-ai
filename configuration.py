2e210ac3a174be6c28a8c551492dd5121dff19bd
srgsuh <srg.suhoverhov@gmail.com>
2025-12-09 19:12:19 +0200

    Remove unused methods from config

--- configuration.py
+++ configuration.py
@@ -11,9 +11,3 @@ def get_config_parameter(parameter_name: str, default_value: str | None = None) 
     if parameter is None:
         raise ConfigurationError(f"Missing configuration for the parameter: {parameter_name}")
     return parameter
-
-def get_api_key() -> str:
-    return get_config_parameter("API_KEY")
-
-def get_url() -> str:
-    return get_config_parameter("URL_CURRENT")
