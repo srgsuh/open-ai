07274cdacabe687e294e4b613787f4657f0910b0
srgsuh <srg.suhoverhov@gmail.com>
2025-12-10 17:06:22 +0200

    Implement response validation error handling

--- exception_handlers.py
+++ exception_handlers.py
@@ -3,16 +3,25 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
 async def request_validation_handler(request: Request, exc: Exception) -> JSONResponse:
-     if isinstance(exc, RequestValidationError):
+    if isinstance(exc, RequestValidationError):
         return JSONResponse(
             status_code=400,
             content={
                 "detail": exc.errors()
             }
         )
-     else:
-         raise exc
-
+    raise exc
+     
+async def response_validation_handler(request: Request, exc: Exception) -> JSONResponse:
+    if isinstance(exc, RequestValidationError):
+        return JSONResponse(
+            status_code=500,
+            content={
+                "detail": exc.errors()
+            }
+        )
+    raise exc
 
 def setup_exception_handlers(app: FastAPI):
-    app.add_exception_handler(exc_class_or_status_code=RequestValidationError, handler=request_validation_handler)
+    app.add_exception_handler(RequestValidationError, request_validation_handler)
+    app.add_exception_handler(RequestValidationError, response_validation_handler)
