cb889c98d35de1c213a5b8ea7c606a4270fa595d
srgsuh <srg.suhoverhov@gmail.com>
2025-12-10 16:39:21 +0200

    Integrate /travel routes

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from middleware import logging_mw
+from routes.travel import travel_router
 
 app = FastAPI()
+
+app.include_router(travel_router, prefix="/travel")
 
 app.middleware("http")(logging_mw)
 
