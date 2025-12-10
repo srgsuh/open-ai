4133a1918935c3caae8219cb77bc87874bc7b792
srgsuh <srg.suhoverhov@gmail.com>
2025-12-10 17:02:01 +0200

    Add handlers to the main app

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from middleware import logging_mw
 from routes.travel import travel_router
+from exception_handlers import setup_exception_handlers
 
 app = FastAPI()
 
@@ -8,3 +9,4 @@ app.include_router(travel_router, prefix="/travel")
 
 app.middleware("http")(logging_mw)
 
+setup_exception_handlers(app)
