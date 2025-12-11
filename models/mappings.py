2e42745486245a0bdb363adb59bf813c612e3ead
srgsuh <srg.suhoverhov@gmail.com>
2025-12-11 11:52:39 +0200

    Add mappings module for unpacking service response

---
+++ models/mappings.py
@@ -0,0 +1,14 @@
+from TravelResponse import TravelResponse
+from services.complex_service import ComplexTravelData
+
+def travel_response_mapper(data: ComplexTravelData) -> TravelResponse:
+    return TravelResponse(
+        countryFrom=data.country_from,
+        countryTo=data.country_to,
+        capitalTo=data.capital_to,
+        weatherTo=data.weather_to,
+        currencyCodeFrom=data.currency_from_code,
+        currencyCodeTo=data.currency_to_code,
+        currencyNameFrom=data.currency_from_name,
+        currencyNameTo=data.currency_to_name
+    )
