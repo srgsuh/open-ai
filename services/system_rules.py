f4bc0107af1a06c111ba65987d03e2f9f9b83bac
srgsuh <srg.suhoverhov@gmail.com>
2025-12-11 11:41:20 +0200

    Refactor system rules for the usage in complex service

--- services/system_rules.py
+++ services/system_rules.py
@@ -1,39 +1,22 @@
-APP_SYSTEM_CONTENT: str = """
-You are helpful traveling assistant.
+country_from = "country_from"
+currency_from_code = "currency_from_code"
+currency_from_name = "currency_from_name"
+country_to = "country_to"
+currency_to_code = "currency_to_code"
+currency_to_name = "currency_to_name"
+capital_to = "capital_to"
 
-General information:
-1. My country is Israel
-2. The currency code of Israel is "ILS"
+SYSTEM_CONTENT = f"""
+If the user’s request contains the phrase "from '<country_from_name>' to '<country_to_name>'",
+respond **only** with a JSON object in the following format:
 
-The list of available tools:
-1. def travel_info(country_from: str, country_to: str, code_from: str)
-
-Rules:
-1. All tool calls must be returned as EXACT JSON of the following form:
-   {"tool": "<tool_name>", "arguments": {...}}
-   - No backticks
-   - No markdown
-   - No extra text before or after
-   - No commentary
-2. When the user clearly requests travel information using any of these phrases:
-   - "travel to '<country_to>'"
-   - "go to '<country_to>'"
-   - "fly to '<country_to>'"
-   - "visit '<country_to>'"
-   - "information about '<country_to>'"
-   - "about '<country_to>'"
-where '<country_to>' is a real country name, you MUST call the travel_info tool with the following arguments:
-    - country_from - the name of my country (as described if the "General information" section)
-    - country_to - the name of requested country
-    - code_from - the currency code of my country
-"""
-
-INNER_SYSTEM_CONTENT: str = """
-If user requests contains statement "currency of '<country_name>'" where '<country_name>' is a real country name,
-response with JSON of the following format: 
-{
-   "country": ...,
-   "currency_name": ...,
-   "currency_code": ...
-}
+{{
+   "{country_from}": "<country_from_name>",
+   "{currency_from_code}": "<ISO code of the origin country’s currency>",
+   "{currency_from_name}": "<Name of the origin country’s currency>",
+   "{country_to}": "<country_to_name>",
+   "{currency_to_code}": "<ISO code of the destination country’s currency>",
+   "{currency_to_name}": "<Name of the destination country’s currency>",
+   "{capital_to}": "<Capital city of the destination country>"
+}}
 """
