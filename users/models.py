5cc7f4b072ddbe0356f6ac59dbff584fa1d1e672
srgsuh <srg.suhoverhov@gmail.com>
2025-12-12 12:28:27 +0200

    ADd roles to the users

--- users/models.py
+++ users/models.py
@@ -1,5 +1,11 @@
 from pydantic import BaseModel
 
+ADMIN_ROLE: str = "ADMIN"
+USER_ROLE: str = "USER"
+
 class User(BaseModel):
     username: str
     role: str
+
+    def is_admin(self) -> bool:
+        return self.role == ADMIN_ROLE
--- users/repository.py
+++ users/repository.py
@@ -1,14 +1,15 @@
 from typing import Optional
-from users.models import User
+from users.models import User, ADMIN_ROLE, USER_ROLE
 
 
 class UserRepository:
-    def __init__(self):
+    def __init__(self) -> None:
         self.mock_data: dict[str, User] = {
-            "user": User("user", "user"),
-            "admin": User("admin", "admin")
+            "user": User(username="user", role=USER_ROLE),
+            "admin": User(username="admin", role=ADMIN_ROLE)
         }
 
     def get_by_username(self, username: str) -> Optional[User]:
         return self.mock_data.get(username)
+    
     
