eae95e96880fcc6117fabe24ccafb1e66646bcb5
srgsuh <srg.suhoverhov@gmail.com>
2025-12-13 22:48:40 +0200

    Add logging

--- auth/router.py
+++ auth/router.py
@@ -4,11 +4,13 @@ from .repository import get_auth_user
 from .hashing import verify
 from .jwt import issue_token
 from auth.models import LoginRequest, LoginResponse
+from logs import logger
 
 auth_router = APIRouter()
 
 @auth_router.post("/login", response_model=LoginResponse)
-def post_login(request: LoginRequest) -> LoginResponse:
+async def post_login(request: LoginRequest) -> LoginResponse:
+    logger.debug(f"post_login. request={request}")
     auth_user: AuthUser | None = get_auth_user(request.username)
     if auth_user and verify(request.password, auth_user.hashed_password):
         return LoginResponse(token=issue_token(auth_user), token_type="bearer")
