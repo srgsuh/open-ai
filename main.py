e4d8f34f3bc542b6dc02a066b1e0b09a9578fbe3
srgsuh <srg.suhoverhov@gmail.com>
2025-12-13 22:48:08 +0200

    Add auth route

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from middleware import logging_mw
 from routes.travel import travel_router
+from auth import auth_router
 from exception_handlers import setup_exception_handlers
 
 app = FastAPI()
 
 app.include_router(travel_router, prefix="/travel")
+app.include_router(auth_router, prefix="/auth")
 
 app.middleware("http")(logging_mw)
 
