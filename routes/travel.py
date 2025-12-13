6866641eae6334d24545e305f17c9b918af9a4ef
srgsuh <srg.suhoverhov@gmail.com>
2025-12-13 22:48:26 +0200

    Add authorization

--- routes/travel.py
+++ routes/travel.py
@@ -1,15 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from logs import logger
 from models.TravelRequest import TravelRequest
 from models.TravelResponse import TravelResponse
 from services.complex_service import complex_service
 from models.mappings import travel_response_mapper
+from auth import get_current_user
 
 travel_router = APIRouter()
 
 @travel_router.post("/info", response_model=TravelResponse, response_model_exclude_none=True)
-async def post_info(request: TravelRequest) -> TravelResponse:
-    logger.debug(f"post_info. Request: {request}")
+async def post_info(request: TravelRequest, user=Depends(get_current_user)) -> TravelResponse:
+    logger.debug(f"post_info. Request: {request}, user={user}")
     data = complex_service(
         request.countryFrom,
         request.countryTo,
