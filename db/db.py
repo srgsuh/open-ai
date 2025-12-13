7b9fa9f0901a4f81a2c1b933b0405534f706d31b
srgsuh <srg.suhoverhov@gmail.com>
2025-12-13 22:10:26 +0200

    Add hashes to mock user enrties

--- db/db.py
+++ db/db.py
@@ -4,17 +4,17 @@ mock_user_data: dict[str, dict] = {
     "user": {
         "username": "user",
         "role": "USER",
-        "hashed_password": "1"
+        "hashed_password": "$2a$10$.MUNrJCapAU8oOAnP6muLewjywqkzJE7PD4VFipmFJyZvY9AU6Rza"
     },
     "subscriber": {
         "username": "subscriber",
         "role": "USER",
-        "hashed_password": "1"
+        "hashed_password": "$2a$10$un6BRatml4BSZPA0PlI8oebYswVLWe9wq48C6h0v/xRd2W6TY96z2"
     },
     "admin": {
         "username": "admin",
         "role": "ADMIN",
-        "hashed_password": "1"
+        "hashed_password": "$2a$10$JMolw8a3b/trMkdQfxg8jOvlLMY9tNU7FHFeGDgHOuINbR/RG5y4i"
     }
 }
 
