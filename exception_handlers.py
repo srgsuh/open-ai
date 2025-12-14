4f7d9e0a42d52169cf49e3a1fd948005803c761b
srgsuh <srg.suhoverhov@gmail.com>
2025-12-14 12:37:46 +0200

    Improve exception handlers to deal with Value errors

--- exception_handlers.py
+++ exception_handlers.py
@@ -1,27 +1,32 @@
 from fastapi import FastAPI, Request
+from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
+from pydantic import ValidationError
+from logs import logger
 
-async def request_validation_handler(request: Request, exc: Exception) -> JSONResponse:
+async def request_validation_handler(request: Request, exc: Exception) -> JSONResponse:    
+    logger.debug(f"Catch ERROR: {str(exc)}, type={type(exc)}")
     if isinstance(exc, RequestValidationError):
+        logger.debug(f"MATCH ERROR TYPE RESPONSE WITH")
         return JSONResponse(
             status_code=400,
             content={
-                "detail": exc.errors()
+                "detail": jsonable_encoder(exc.errors())
             }
         )
     raise exc
      
 async def response_validation_handler(request: Request, exc: Exception) -> JSONResponse:
-    if isinstance(exc, RequestValidationError):
+    if isinstance(exc, ValidationError):
         return JSONResponse(
             status_code=500,
             content={
-                "detail": exc.errors()
+                "detail": jsonable_encoder(exc.errors())
             }
         )
     raise exc
 
 def setup_exception_handlers(app: FastAPI):
     app.add_exception_handler(RequestValidationError, request_validation_handler)
-    app.add_exception_handler(RequestValidationError, response_validation_handler)
+    app.add_exception_handler(ValidationError, response_validation_handler)
