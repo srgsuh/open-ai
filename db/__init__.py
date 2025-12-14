9cca034aadd36ed20310927e9d503f196e1bb76f
srgsuh <srg.suhoverhov@gmail.com>
2025-12-14 09:00:20 +0200

    Add get all users method and add field for requests count

--- db/__init__.py
+++ db/__init__.py
@@ -1,3 +1,3 @@
-from .db import get_db_user
+from .db import get_db_user, get_all_db_users
 
-__all__ = ["get_db_user"]
+__all__ = ["get_db_user", "get_all_db_users"]
--- db/db.py
+++ db/db.py
@@ -4,21 +4,27 @@ mock_user_data: dict[str, dict] = {
     "user": {
         "username": "user",
         "role": "USER",
-        "hashed_password": "$2a$10$.MUNrJCapAU8oOAnP6muLewjywqkzJE7PD4VFipmFJyZvY9AU6Rza"
+        "hashed_password": "$2a$10$.MUNrJCapAU8oOAnP6muLewjywqkzJE7PD4VFipmFJyZvY9AU6Rza",
+        "requests": 0
     },
     "subscriber": {
         "username": "subscriber",
         "role": "USER",
-        "hashed_password": "$2a$10$un6BRatml4BSZPA0PlI8oebYswVLWe9wq48C6h0v/xRd2W6TY96z2"
+        "hashed_password": "$2a$10$un6BRatml4BSZPA0PlI8oebYswVLWe9wq48C6h0v/xRd2W6TY96z2",
+        "requests": 0
     },
     "admin": {
         "username": "admin",
         "role": "ADMIN",
-        "hashed_password": "$2a$10$JMolw8a3b/trMkdQfxg8jOvlLMY9tNU7FHFeGDgHOuINbR/RG5y4i"
+        "hashed_password": "$2a$10$JMolw8a3b/trMkdQfxg8jOvlLMY9tNU7FHFeGDgHOuINbR/RG5y4i",
+        "requests": 0
     }
 }
 
 def get_db_user(username: str) -> Optional[dict]:
     return mock_user_data.get(username)
+
+def get_all_db_users() -> list[dict]:
+    return [v for v in mock_user_data.values()]
 
 __all__ = ["get_db_user"]
