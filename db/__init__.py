fe8d07bfa43d1bc0ada248e9191fa5a357e05703
srgsuh <srg.suhoverhov@gmail.com>
2025-12-14 09:16:13 +0200

    Add stat update function

--- db/__init__.py
+++ db/__init__.py
@@ -1,3 +1,3 @@
-from .db import get_db_user, get_all_db_users
+from .db import get_db_user,get_all_db_users,get_statistics,increase_statistics
 
-__all__ = ["get_db_user", "get_all_db_users"]
+__all__ = ["get_db_user", "get_all_db_users", "get_statistics", "increase_statistics"]
--- db/db.py
+++ db/db.py
@@ -4,22 +4,21 @@ mock_user_data: dict[str, dict] = {
     "user": {
         "username": "user",
         "role": "USER",
-        "hashed_password": "$2a$10$.MUNrJCapAU8oOAnP6muLewjywqkzJE7PD4VFipmFJyZvY9AU6Rza",
-        "requests": 0
+        "hashed_password": "$2a$10$.MUNrJCapAU8oOAnP6muLewjywqkzJE7PD4VFipmFJyZvY9AU6Rza"
     },
     "subscriber": {
         "username": "subscriber",
         "role": "USER",
-        "hashed_password": "$2a$10$un6BRatml4BSZPA0PlI8oebYswVLWe9wq48C6h0v/xRd2W6TY96z2",
-        "requests": 0
+        "hashed_password": "$2a$10$un6BRatml4BSZPA0PlI8oebYswVLWe9wq48C6h0v/xRd2W6TY96z2"
     },
     "admin": {
         "username": "admin",
         "role": "ADMIN",
-        "hashed_password": "$2a$10$JMolw8a3b/trMkdQfxg8jOvlLMY9tNU7FHFeGDgHOuINbR/RG5y4i",
-        "requests": 0
+        "hashed_password": "$2a$10$JMolw8a3b/trMkdQfxg8jOvlLMY9tNU7FHFeGDgHOuINbR/RG5y4i"
     }
 }
+
+statistics: dict[str, int] = {}
 
 def get_db_user(username: str) -> Optional[dict]:
     return mock_user_data.get(username)
@@ -27,4 +26,12 @@ def get_db_user(username: str) -> Optional[dict]:
 def get_all_db_users() -> list[dict]:
     return [v for v in mock_user_data.values()]
 
-__all__ = ["get_db_user"]
+def get_statistics(role: str = "") -> dict[str, int]:
+    return {
+        k: statistics.get(k, 0) for k in mock_user_data if (not role or mock_user_data[k]["role"] == role)
+    }
+
+def increase_statistics(username: str) -> None:
+    statistics[username] = 1 + statistics.get(username, 0)
+
+__all__ = ["get_db_user", "get_all_db_users", "get_statistics", "increase_statistics"]
