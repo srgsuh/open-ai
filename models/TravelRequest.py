f0422d030f575de620753b0293b37c9afe941389
srgsuh <srg.suhoverhov@gmail.com>
2025-12-14 10:48:06 +0200

    Add field cityTo and its validation

--- models/TravelRequest.py
+++ models/TravelRequest.py
@@ -1,8 +1,19 @@
-from pydantic import BaseModel
+from typing import Self
+from pydantic import BaseModel, model_validator
 
 class TravelRequest(BaseModel):
     countryFrom: str
-    countryTo: str
+    countryTo: str | None = None
+    cityTo: str | None = None
     isCapital: bool | None = None
     isWeather: bool | None = None
     isCurrency: bool | None = None
+
+    @model_validator(mode="after")
+    def validate_destination(self) -> Self:
+        if bool(self.countryTo) == bool(self.cityTo):
+            raise ValueError('Exactly one of the fields "countryTo", "cityTo" must be provided')
+        if bool(self.isCapital) and not bool(self.countryTo):
+            raise ValueError('Field "isCapital" can only be provided with the field "countryTo"')
+
+        return self
