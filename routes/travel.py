8b37f32af9fcadcb825aba3bb61635eacb823cd1
srgsuh <srg.suhoverhov@gmail.com>
2025-12-14 18:46:31 +0200

    Refactor get_info to use TravelRequest for the validation purposes

--- routes/travel.py
+++ routes/travel.py
@@ -19,7 +19,7 @@ async def post_info(request: TravelRequest) -> TravelResponse:
     return travel_response_mapper(data, request.cityTo, bool(request.isCapital), bool(request.isWeather), bool(request.isCurrency))
 
 @travel_router.get("/info", response_model=TravelResponse, response_model_exclude_none=True)
-async def get_info(countryFrom: str, countryTo: str) -> TravelResponse:
-    logger.debug(f"get_info. countryFrom: {countryFrom}, countryTo: {countryTo}")
-    data = complex_service(countryFrom, countryTo)
+async def get_info(request: TravelRequest = Depends()) -> TravelResponse:
+    logger.debug(f"get_info. Request: {request}")
+    data = complex_service(request.countryFrom, request.countryTo, request.cityTo)
     return travel_response_mapper(data)
