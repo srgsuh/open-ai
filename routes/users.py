addcccf4ec95829e245ba94dbbe4692fea5aef40
srgsuh <srg.suhoverhov@gmail.com>
2025-12-14 10:24:33 +0200

    Remove stat collection from the users route

--- routes/users.py
+++ routes/users.py
@@ -10,7 +10,7 @@ class UserStatistics(BaseModel):
     username: str
     requests: int
 
-users_router = APIRouter(dependencies=[Depends(collect_user_statistics)])
+users_router = APIRouter()
 @users_router.get("/statistics", response_model=dict[str, int])
 async def get_users_statistics(role: Optional[str] = None, user: User = Depends(require_admin)) -> dict[str, int]:
     logger.debug(f"get_users_statistics. role={role}, user={user}")
