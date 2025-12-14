1a345c1966a515a2358c806fc9976a36ba6204fb
srgsuh <srg.suhoverhov@gmail.com>
2025-12-14 12:08:01 +0200

    Fix rules to include option of city to

--- services/system_rules.py
+++ services/system_rules.py
@@ -7,14 +7,14 @@ currency_to_name = "currency_to_name"
 capital_to = "capital_to"
 
 SYSTEM_CONTENT = f"""
-If the user’s request contains the phrase "from '<country_from_name>' to '<country_to_name>'",
+If the user’s request contains the phrase "from '<country_from_name>' to '<where_to_name>'",
+and '<where_to_name>' is a real country name or a city_name
 respond **only** with a JSON object in the following format:
-
 {{
    "{country_from}": "<country_from_name>",
    "{currency_from_code}": "<ISO code of the origin country’s currency>",
    "{currency_from_name}": "<Name of the origin country’s currency>",
-   "{country_to}": "<country_to_name>",
+   "{country_to}": "<Name of the destination country>",
    "{currency_to_code}": "<ISO code of the destination country’s currency>",
    "{currency_to_name}": "<Name of the destination country’s currency>",
    "{capital_to}": "<Capital city of the destination country>"
