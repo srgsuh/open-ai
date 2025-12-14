a5e7a5194fe79fbf9ebdb2f9dda80b4404a6235f
srgsuh <srg.suhoverhov@gmail.com>
2025-12-14 09:37:13 +0200

    Attach users router

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from middleware import logging_mw
 from routes.travel import travel_router
+from routes.users import users_router
 from auth import auth_router
 from exception_handlers import setup_exception_handlers
 
 app = FastAPI()
 
 app.include_router(travel_router, prefix="/travel")
+app.include_router(users_router, prefix="/users")
 app.include_router(auth_router, prefix="/auth")
 
 app.middleware("http")(logging_mw)
