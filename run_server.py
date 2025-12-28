8a704a341b89fc83207ebfb4d6a91becdd35f293
Sergey Suhoverhov <srg.suhoverhov@gmail.com>
2025-12-28 14:29:24 +0200

    Add port loading from environment variables

--- run_server.py
+++ run_server.py
@@ -1,10 +1,18 @@
 import uvicorn
+from configuration import get_config_parameter
+from logs import logger
+
+DEFAULT_PORT: int = 8000
+
+port: int = int(get_config_parameter("PORT", "8000"))
 
 if __name__ == "__main__":
+    port: int = int(get_config_parameter("PORT", "8000"))
+    logger.debug(f"Running a server on a port {port}")
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8000,
+        port=port,
         reload=True,
         workers=4,
         log_level="info"
