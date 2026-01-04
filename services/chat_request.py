9970d118723ce9c48d4b4a40b1b812b69c5f3038
Sergey Suhoverhov <srg.suhoverhov@gmail.com>
2026-01-04 12:26:35 +0200

    Make ollama model url and model_name configurable parameters

--- services/chat_request.py
+++ services/chat_request.py
@@ -1,9 +1,16 @@
 import requests
 from typing import Any, Self
 from services.extract_json import extract_json
+from configuration import get_config_parameter
 
 URL: str = "http://localhost:11434/api/chat"
 MODEL_NAME: str = "phi3"
+
+def __get_url() -> str:
+    return get_config_parameter("OLLAMA_URL", URL)
+
+def __get_model_name() -> str:
+    return get_config_parameter("OLLAMA_MODEL", MODEL_NAME)
 
 class ChatHistory:
     SYS_ROLE: str = "system"
@@ -20,12 +27,13 @@ class ChatHistory:
     
 class ChatLLM:
     def __init__(self, system_content: str, **options) -> None:
-        self.__url = URL
+        self.__url = __get_url()
+        self.__model_name = __get_model_name()
         self.__history = ChatHistory(system_content)
         if not "temperature" in options:
             options["temperature"] = 0.0
         self.__base_payload: dict[str, Any] = {
-            "model": MODEL_NAME,
+            "model": self.__model_name,
             "stream": False,
             "options": options,
             "messages": self.__history.messages
