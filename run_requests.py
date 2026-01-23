adaebf8b21c124a8cf7c5cdeb6ecc1d9b016c0a0
Sergey Suhoverhov <srg.suhoverhov@gmail.com>
2026-01-23 12:33:55 +0200

    Modify to send requests over a long period of time

--- run_requests.py
+++ run_requests.py
@@ -1,8 +1,9 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
+import time
 from requests import Session, Response, RequestException
 
 URL: str = "https://www.sergeysuhoverhov.space/api/hello"
-REQUEST_COUNT: int = 64
+DEFAULT_REQUEST_COUNT: int = 128
 MAX_WORKERS: int = 4
 
 def one_request(session: Session, url: str, request_id) -> tuple[int, int]:
@@ -12,16 +13,25 @@ def one_request(session: Session, url: str, request_id) -> tuple[int, int]:
     except RequestException as re:
         return request_id, -1
 
-def spam_requests() -> None:
+def spam_requests(request_count: int = DEFAULT_REQUEST_COUNT) -> None:
     with Session() as session:
         with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
             future_tasks = [
                 executor.submit(one_request, session, URL, req_no)
-                for req_no in range(1, REQUEST_COUNT + 1)
+                for req_no in range(1, request_count + 1)
             ]
             for made_tasks in as_completed(future_tasks):
                 id, status = made_tasks.result()
                 print(f'Request id={id} is finished with the status code={status}')
 
 if __name__ == "__main__":
+    NUM_OF_MINUTES = 5
+    TIMES_PER_MINUTE = 5
+    time_to_sleep = 60.0/TIMES_PER_MINUTE
+    for minute in range(NUM_OF_MINUTES):
+        for episode in range(TIMES_PER_MINUTE):
+            print(f"Minute {minute}, episode {episode}. Run session:")
+            spam_requests(32)
+            print(f"Into a sleep mode for {time_to_sleep} seconds...")
+            time.sleep(time_to_sleep)
     spam_requests()
